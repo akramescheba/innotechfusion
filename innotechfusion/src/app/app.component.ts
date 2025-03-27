4691711b69472be999c281ba472688bece58a637
akramescheba <meschebajordy@gmail.com>
2025-03-27 22:42:51 +0100

    Ajout de la fonctionnalité de vote et de la sauvegarde d'un vote avec localStorage

--- innotechfusion/src/app/app.component.ts
+++ innotechfusion/src/app/app.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {CommonModule} from "@angular/common";
 import { RouterOutlet } from '@angular/router';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'innotechfusion';
+
+  constructor(private http: HttpClient){}
+
+  list:any[]=[];
+
+  voted: {[id:number]: boolean}={};
+
+  ngOnInit(): void{
+    this.http.get<any>("http://localhost:8082/membres").subscribe((data)=>
+    this.list=data)
+    const vote = localStorage.getItem('Token');
+    
+    if (vote) {
+      this.voted = JSON.parse(vote);
+    }
+  }
+
+  voter(id:number): void{
+    this.voted[id]=true;
+    localStorage.setItem('Token', JSON.stringify(this.voted))
+  }
+  
+
 }
