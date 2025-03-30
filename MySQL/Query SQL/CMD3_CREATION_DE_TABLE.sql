8a10730525cc599b73f6ea2f9530eacca33b263f
akramescheba <meschebajordy@gmail.com>
2025-03-30 18:30:21 +0200

    Ajout des Query SQL

---
+++ MySQL/Query SQL/CMD3_CREATION_DE_TABLE.sql
@@ -0,0 +1,6 @@
+    CREATE TABLE membre (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    ddn INT NOT NULL,
+    nom VARCHAR (50) NOT NULL,
+    prenom VARCHAR (50) NOT NULL,
+    vote VARCHAR (50) NOT NULL,);
---
+++ MySQL/Query SQL/CMD4_INSERTION_DE_DONNEES.sql
@@ -0,0 +1,7 @@
+INSERT INTO membre (id, ddn, nom, prenom, vote) 
+VALUES (5, 2002, 'Moreau', 'Lucas', 'voter'),(6, 1996, 'Lefevre', 'Emma', 'voter'),
+(7, 1990, 'Bernard', 'Sophie', 'voter'),(8, 1985, 'Dubois', 'Thomas', 'voter'),
+(9, 1998, 'Martinez', 'Camille', 'voter'),(10, 1993, 'Garcia', 'Hugo', 'voter'),
+(11, 2000, 'Lambert', 'Manon', 'voter'),(12, 1987, 'Fontaine', 'Louis', 'voter'),
+(13, 1995, 'Rousseau', 'Chloé', 'voter'), (14, 2001, 'Blanc', 'Nathan', 'voter'
+);
